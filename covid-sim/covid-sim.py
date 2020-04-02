99db5fd535965e3bf6839e103b6f286ebd99758b
Will Rhodes <astro491@gmail.com>
2020-04-01 21:27:24 -0400

    refactoring

--- covid-sim/covid-sim.py
+++ covid-sim/covid-sim.py
@@ -10,44 +10,39 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import cartopy.crs as ccrs
 import cartopy.io.shapereader as shpreader
-from scipy.optimize import OptimizeWarning
+from scipy.optimize import OptimizeWarning,fsolve
 
 warnings.simplefilter("ignore", OptimizeWarning)
 COUNTRY_POPULATIONS = {"China":1433783686, "US":329064917, "Japan":126860301, "United Kingdom":67530172, "Italy":60550075, "Canada":37411047}
 COUNTRY_COLORS =  {'China':'orange','US':'blue','Japan':'yellow','United Kingdom':'purple', 'Italy':'green', 'Canada':'red'}
 STATE_POPULATIONS = {"California":39512223,"Texas":28995881,"Florida":21477737,"New York":19453561,"Pennsylvania":12801989,"Illinois":12671821,"Ohio":11689100,"Georgia":10617423,"North Carolina":10488084,"Michigan":9986857,"New Jersey":8882190,"Virginia":8535519,"Washington":7614893,"Arizona":7278717,"Massachusetts":6949503,"Tennessee":6833174,"Indiana":6732219,"Missouri":6137428,"Maryland":6045680,"Wisconsin":5822434,"Colorado":5758736,"Minnesota":5639632,"South Carolina":5148714,"Alabama":4903185,"Louisiana":4648794,"Kentucky":4467673,"Oregon":4217737,"Oklahoma":3956971,"Connecticut":3565287,"Utah":3205958,"Puerto Rico":3193694,"Iowa":3155070,"Nevada":3080156,"Arkansas":3017825,"Mississippi":2976149,"Kansas":2913314,"New Mexico":2096829,"Nebraska":1934408,"West Virginia":1792065,"Idaho":1787147,"Hawaii":1415872,"New Hampshire":1359711,"Maine":1344212,"Montana":1068778,"Rhode Island":1059361,"Delaware":973764,"South Dakota":884659,"North Dakota":762062,"Alaska":731545,"District of Columbia":705749,"Vermont":623989,"Wyoming":578759}
 
-def logistic(x,l,k,o):
-    return l / (1 + numpy.exp(k*(o-x)))
+logistic = lambda x,l,k,o: l / (1 + numpy.exp(k*(o-x)))
 
-def expo(x,a,b):
-    return a* numpy.exp(b*x)
+expo = lambda x,a,b: a* numpy.exp(b*x)
 
 FORMULAE = {
     logistic:'$y=\frac{{{0:.3f}}}{{1+e^{{{1:.3f}({2:.3f}-x)}}}}$',
     expo:'$y={0:.3f}e^{{{1:.3f}x}}$'
 }
 
-def cmap(value,mn,mx):
-    value = numpy.abs(value)
-    mn = numpy.abs(mn)
-    mx = numpy.abs(mx)
-    bg = 1-((value-mn)/(mx-mn)) #pow(scale,1-normalized)/scale
-    return [1,bg,bg]
-
 def start():
     PATH_BASE = '/home/will/Projects/COVID-19/csse_covid_19_data/csse_covid_19_time_series'
     OUTPUT_BASE = '/home/will/Projects/PythonUtilities/covid-sim/out/'
     PATH_TIME_CONFIRMED = os.path.join(PATH_BASE,'time_series_19-covid-Confirmed.csv')
 
+    duration = 120
     important_states = ['New York','New Jersey','Pennsylvania']
+    func = logistic
+    bounds=([0.03,-1,5], [0.90,1,duration])
 
-    #custom_StatesDeaths(PATH_TIME_CONFIRMED,PATH_TIME_DEATHS)
     custom_StatesNew(PATH_TIME_CONFIRMED,important_states)
     custom_StatesPerMap(PATH_TIME_CONFIRMED)
-    custom_StatesFitMap(PATH_TIME_CONFIRMED,OUTPUT_BASE, important_states)
     custom_CountriesPerZero(PATH_TIME_CONFIRMED)
-    custom_StatesExtrapolate(PATH_TIME_CONFIRMED)
+
+    custom_StatesFitMap(PATH_TIME_CONFIRMED,OUTPUT_BASE, important_states,func,bounds)
+
+    custom_StatesExtrapolate(PATH_TIME_CONFIRMED,important_states,duration,func,bounds)
     
 
 def custom_UsaRecovery(path,recoverypath):
@@ -86,7 +81,7 @@ def custom_StatesNew(path,check_states=[],min_cases=0):
     
     ax.set_xlabel('Days since at least {} cases in that state'.format(min_cases))
     ax.set_ylabel('New Cases since Previous Day')
-    ax.set_title('New Cases Per Day for Various States'.format(min_cases))
+    ax.set_title('New Cases Per Day for Various States')
     ax.legend()
     plt.show()       
 
@@ -126,7 +121,7 @@ def custom_StatesFitMap(path,OUTPUT_BASE,check_states=[],func=expo,min_days=3):
                 except:
                     raise ValueError('Could not find best fit for {}'.format(state))
 
-    us_map(states,'Case Growth, b in $ae^{{bx}}$')
+    us_map(states,'Case Growth')
 
 
 def custom_CountriesPerZero(path,func=expo,min_cases=100):
@@ -150,39 +145,40 @@ def custom_CountriesPerZero(path,func=expo,min_cases=100):
     plt.show()
 
 
-def custom_StatesExtrapolate(path,func=expo,state='Pennsylvania',duration = 90):
+def custom_StatesExtrapolate(path,important_states,duration = 90,func=logistic, bounds=None):
     '''
-    Extrapolates out until 100%
+    Extrapolates the current data to fit func
     '''
-    _, ax = plt.subplots()
-    x,y = parse_time(path,province=state)
-    y = [i/STATE_POPULATIONS[state]*100. for i in y if i>0]
-    xd = x[-len(y):]
-    x = range(len(y))
-    ax.plot(xd,y,c='red',linewidth=3)
+    fig = plt.figure(figsize=(8, 6))
+    for i,state in enumerate(important_states):
+        ax = fig.add_subplot(220+(i+1))
+        x,y = parse_time(path,province=state)
+        y = [i/STATE_POPULATIONS[state]*100. for i in y if i>0]
+        xd = x[-len(y):]
+        x = range(len(y))
+        ax.plot(xd,y,c='red',linewidth=3)
 
-    params,_ = curve_fit(func, x, y)
-    formula = FORMULAE[func].format(*params)
-    ax.text(0,max(y),formula)
-    print(formula)
-    xd = get_date_range(xd[0],duration)
-    x = range(duration)
-    y = func(x,*params)
-    
-    y = [i for i in y if i<=100]
-    x = x[0:len(y)]
-    xd = xd[0:len(y)]
+        params,_ = curve_fit(func, x, y,bounds=bounds)
+        formula = FORMULAE[func].format(*params)
+        ax.text(0,max(y),formula)
 
-    ax.plot(xd,y,c='blue',dashes=[6, 2],linewidth=1)
+        xd = get_date_range(xd[0],duration)
+        x = range(duration)
+        y = func(x,*params)
+        
+        y = [i for i in y if i<=100]
+        x = x[0:len(y)]
+        xd = xd[0:len(y)]
 
-    ax.set_xlabel('Date')
-    plt.xticks(rotation=30)
+        ax.plot(xd,y,c='blue',dashes=[6, 2],linewidth=1)
 
-    ax.set_yscale('log')    
-    ax.set_ylabel('% of Population Infected')
-    ax.yaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter('{x:3}'))
-    
-    ax.set_title('Extrapolation to 100% Infected in {}'.format(state))
+        ax.set_xlabel('Date')
+        plt.xticks(rotation=30)
+
+        ax.set_ylabel('% of Population Infected')
+        ax.yaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter('{x:.3f}'))
+        
+        ax.set_title('Extrapolation of Data in {}'.format(state))
     plt.show()
 
 
@@ -314,5 +310,15 @@ def us_map(states,title = '',text_top=5,formatter = '{0:.3f}'):
             ax.text(x, y, formatter.format(value),size=7, color='blue', ha='center', va='center', transform=ccrs.PlateCarree())   
             
     plt.show()
+
+
+def cmap(value,mn,mx):
+    value = numpy.abs(value)
+    mn = numpy.abs(mn)
+    mx = numpy.abs(mx)
+    bg = 1-((value-mn)/(mx-mn)) #pow(scale,1-normalized)/scale
+    return [1,bg,bg]
+
+
 
 if  __name__ =='__main__':start()
