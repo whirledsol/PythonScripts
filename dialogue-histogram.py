3bc617a1f71ae2908d79d395d7906021a03a4e1d
Will Rhodes <astro491@gmail.com>
2020-07-19 22:34:03 -0400

    change to dialogue-histogram.py

--- dialogue-histogram.py
+++ dialogue-histogram.py
@@ -5,44 +5,77 @@ Created: 20190118
 @author: Will Rhodes
 
 """
-import pysrt
-import seaborn 
-import numpy
-import math
-import matplotlib.pyplot as pyplot
+import os,pysrt,seaborn,numpy,math
+import matplotlib.pyplot as plt
+
+
 
 def start():
-	filename = 'assets/2001.srt'
-	title = '"2001: A Space Odyssey" Dialogue'
+	directory = 'assets/atla_s03/'
+	title = 'Avatar Season 3 Dialogue'
+	color='red'
 	binsize = 1
 	tickspacing = 5
-	duration = 142
-	bins = parse(filename,binsize,duration)
-	graph(bins,title,tickspacing)
-	
 
-def parse(filename,binsize,duration):
-	subs = pysrt.open(filename)
+	files = os.listdir(directory)
+	files.sort()
+
+	rows = 3 if len(files) % 3 == 0 else 2
+	cols = int(math.ceil(len(files)/rows))
+	
+	print(f"About to graph subplots ({rows},{cols})")
+	fig, axs = plt.subplots(rows, cols)
+	axs = flatten(axs)
+	for i,ax in enumerate(axs):
+		if(i<len(files)):
+			filename = files[i]
+			filename = os.path.join(directory,filename)
+			subtitle = os.path.basename(filename)
+
+			#custom
+			subtitle = subtitle.split('-')[-1].split('.')[0].strip()
+
+			bins = parse(filename,binsize)
+			graph_subplot(ax,bins,tickspacing,color,subtitle)
+		else:
+			ax.axis('off')
+	fig.suptitle(title)
+	plt.subplots_adjust(left=.02, top=.85, right=.98, bottom=.04)
+	plt.show()
+
+def parse(filename,binsize,duration=None):
+	'''
+	parse the srt
+	'''
+	subs = pysrt.open(filename, encoding='iso-8859-1')
 	sample = [x.text for x in subs[:-50]]
-	print('SAMPLE')
-	print(sample)
+	#print('SAMPLE')
+	#print(sample)
 
 	timestamps = [x.start.ordinal/1000/60 for x in subs]
 	mx = int(math.ceil(max(timestamps))) if duration is None else duration
 	binsize = int(mx/binsize)
 	return numpy.histogram(timestamps,binsize,range=(0,mx))
 
-
-def graph(bins,title,tickspacing):
+def graph_subplot(ax, bins,tickspacing, color='red', subtitle=None):
+	'''
+	graphs the data for the current plot
+	'''
 	x = bins[1][:-1]
 	y = bins[0]
 	ticks = numpy.arange(max(x),step=tickspacing)
-	palette = seaborn.dark_palette("red")
-	ax = seaborn.barplot(x,y,palette=palette)
+	palette = seaborn.dark_palette(color)
+	seaborn.barplot(x,y,ax=ax,palette=palette)
 	ax.set_xticks(ticks)
-	ax.set_xticklabels([int(x) for x in ticks])
-	ax.set_xlabel('Time (minutes)')
-	ax.set_ylabel('Lines')
-	ax.set_title(title)
-	pyplot.show()
+	
+	#custom: no values
+	ax.set_yticklabels([])
+	ax.set_xticklabels([])
+	#ax.set_xticklabels([int(x) for x in ticks])
+
+	if subtitle is not None:
+		ax.set_title(subtitle, fontsize=6)
+
+def flatten(l): return [item for sublist in l for item in sublist]
+
 if  __name__ =='__main__': start()
