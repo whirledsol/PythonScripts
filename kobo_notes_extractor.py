4a1e3f93704c593fb7dc12aae3d2f963cb2a8b33
whirledsol <astro491@gmail.com>
2022-06-19 18:03:45 -0400

    best algorithm so far

--- kobo_notes_extractor.py
+++ kobo_notes_extractor.py
@@ -47,20 +47,27 @@ def main():
 	print(f"Found content Id {contentId}")
 
 	bookmarks = get_content_bookmarks(cur,contentId)
+	
+	#sort in reverse
+	bookmarks.sort(key=get_point_coordinates_sort, reverse=True)
+
 	print(f"Found {len(bookmarks)} bookmarks")
 	#print(bookmarks[0].toJSON())
 
 	soup = epub_to_soup(PATH_IN_EPUB,True)
 	
 	#add marks in reverse order so location-finding works
-	for bookmark in bookmarks:
-		soup = insert_bookmark(soup,bookmark)
-		pass
+	for bookmark in bookmarks[0:5000]:
+		if bookmark.Text:
+			soup = insert_bookmark(soup,bookmark)
+			pass
 
 
 	#add bootstrap
 	soup = add_bootstrap(soup)
 	soup = add_navigation(soup)
+	#soup = add_sidebar(soup) #does not cooperate with popover
+
 	#write
 	write_html(soup.prettify(),PATH_OUT_HTML)
 
@@ -78,7 +85,7 @@ def get_content_bookmarks(cur,ContentID):
 	'''
 	Finds and ORMs all bookmarks for supplied content ID with the weirdness that the volumeID is the contentID
 	'''
-	rows = cur.execute("select * from Bookmark WHERE VolumeID=:ContentID ORDER BY DateCreated desc",{"ContentID":ContentID}).fetchall()
+	rows = cur.execute("select * from Bookmark WHERE VolumeID=:ContentID",{"ContentID":ContentID}).fetchall()
 	return [Bookmark(x) for x in rows]
 
 
@@ -92,6 +99,7 @@ def epub_to_html(path):
 		if item.get_type() in [ebooklib.ITEM_DOCUMENT]:
 			clean = item.get_content().decode("utf-8")
 			chapters.append(clean)
+
 	return ''.join(chapters)
 
 
@@ -101,9 +109,10 @@ def epub_to_soup(path,removeCover=False):
 	'''	
 	html = epub_to_html(path)
 	soup = BeautifulSoup(html, 'html.parser')
-	[x.extract() for x in soup.find_all('a')]
 	if removeCover:
 		soup.find('html').extract()
+		
+	#[x.extract() for x in soup.find_all('a')]
 	return soup
 
 
@@ -136,7 +145,7 @@ def add_navigation(soup):
 	<div class="bg-light fixed-top w-100 p-2 text-end">
 	<span id="annotation-current"></span>
 	<button type="button" id="btn-prev" class="btn btn-sm btn-outline-dark">Previous</button>
-	<button type="button" id="btn-next" class="btn btn-sm  btn-outline-primary">Next</button>
+	<button type="button" id="btn-next" class="btn btn-sm  btn-primary">Next</button>
 	</div>
 	<script>
 		var annotationIndex = -1;
@@ -179,6 +188,28 @@ def add_navigation(soup):
 	soup.find_all('body')[-1].append(navigation)
 	return soup
 
+def add_sidebar(soup):
+	'''
+	adds navigation pane
+	'''
+	script = '''
+	<script>
+		if(annotations){
+			let sideItems = '';
+			annotations.map((el,i)=>{
+				const y = el.getBoundingClientRect().top;
+				const title = el.dataset.title;
+				console.log(title,y);
+				sideItems += '<div class="position-absolute" style="top:'+y+'px;left:0;color:#999">'+title+'</div>';
+			});
+			document.body.innerHTML += sideItems
+		}
+	</script>
+	'''
+	script = BeautifulSoup(script, 'html.parser')
+	soup.find_all('body')[-1].append(script)
+	return soup
+
 
 def insert_bookmark(soup,bookmark):
 	'''
@@ -194,84 +225,95 @@ def insert_bookmark(soup,bookmark):
 
 		popover = get_bookmark_tag(soup,bookmark)
 		
-		if(coordinatesStart[1] == coordinatesEnd[1]):
-			soup = inject_popover(soup,coordinatesStart,coordinatesEnd,popover)
-		else:
-			soup = wrap_popover(soup,coordinatesStart,coordinatesEnd,popover)
+		soup = inject_popover(soup,coordinatesStart,coordinatesEnd,popover)
+		
 	except Exception as e:
-		print(f"\n ERROR INSERTING BOOKMARK {bookmark.BookmarkID}")
-		print(bookmark.toJSON())
+		#print(f"\n ERROR INSERTING BOOKMARK {bookmark.BookmarkID}")
+		#print(bookmark.toJSON())
 		print(traceback.format_exc())
 		
 	return soup
 
+def get_element_content(element):
+	'''
+	could be tag or navigablestring
+	'''
+	return (element.string or '') if isinstance(element,Tag) else str(element)
 
-def locate_content(soup,documentIndex,index):
+
+def is_element_empty(element):
+	'''
+	returns bool if empty
+	'''
+	return len(get_element_content(element).strip(' \t\n\r')) == 0
+
+
+def locate_element(soup,documentIndex,index):
 	'''
 	locates the tag
 	'''
 	document = soup.find_all('body')[documentIndex]
 	
 	documentElements = list(document.children)
-	content = documentElements[index]
+	element = documentElements[index]
 
-
-	if(len(str(content).strip()) == 0):
-		#we are in a liminal space, take heed, move back one
-		#content = content.next_sibling
-		raise "fix this"
-		print('No content. Using next sibling.')
-		pass
-
-	if content.string is None:
-		content.string = ""
-	return content
-
-
-def wrap_popover(soup,coordinatesStart,coordinatesEnd,popover):
-	'''
-	wraps multiple elements
-	'''
-	#print('\twrap')
-	documentIndex, indexStart, charStart = coordinatesStart
-	_,indexEnd,charEnd = coordinatesEnd
-	'''
-	#creates multiple wrappers and just doesn't work well enough
-	for i in range(0,indexEnd-indexStart+1):
-		content = locate_content(soup,documentIndex,indexEnd-i)
-		content.wrap(popover)
-	'''
-	content = locate_content(soup,documentIndex,indexStart)
-	content.wrap(popover)
-	return soup
+	return element
 
 
 def inject_popover(soup,coordinatesStart,coordinatesEnd,popover):
 	'''
 	wraps text within element
 	'''
-	#print('\tinject')
-	documentIndex, index, charStart = coordinatesStart
-	_,_,charEnd = coordinatesEnd
-	content = locate_content(soup,documentIndex,index)
+	title = popover['title']
 
-	if(isinstance(content,NavigableString)):
-		return wrap_popover(soup,coordinatesStart,coordinatesEnd,popover)
+	documentIndex, indexStart, charStart = coordinatesStart
+	_,indexEnd,charEnd = coordinatesEnd
 
-	if(popover['title'] == 'c0b813bf-5f6f-4a4e-ae8a-111d85f46eff'):
-		print(len(list(content.children)))
+	#which index to use?
+	index = indexStart if charEnd == 0 else indexEnd
 
-	start = content.string[:charStart] or ""
-	middle = content.string[charStart:charEnd] or ""
-	end = content.string[charEnd:] or ""
-
+	element = locate_element(soup,documentIndex,index)
 	
+	
+
+	if(isinstance(element,NavigableString)):
+		print(title,'wrap')
+		popover['class'] += ' d-inline-block'
+		element.wrap(popover)
+		return soup
+
+
+	#based on the flat hierarchy by default, there will either be one element and this works
+	#OR (since we are going in reverse order) multiple elements, and we'll take the first one - undoubetedly a navigablestring
+	content = element.contents[0]
+	if(len(element.contents) > 1 and isinstance(content,Tag) and content.name == 'a' and content.string is None):
+		#this is a page tag, ignore
+		content = element.contents[1]
+	content = str(content)
+
+	#which char range?
+	charStart = 0 if charStart > charEnd else charStart
+	charEnd = len(content)-1 if charEnd == 0 else charEnd
+
+	if title == '772d7a31-de6f-4532-81e1-c7660ec0410f':
+		print(title,'inject',charStart,charEnd,len(content),content,'\n')	
+
+
+	#seperate
+	start = BeautifulSoup((content[:charStart] or ""), 'html.parser')
+	middle = content[charStart:charEnd] or ""
+	end = BeautifulSoup((content[charEnd:] or ""), 'html.parser')
 
 	popover.string = middle
 
-	content.string = start
-	content.append(popover)
-	content.append(end)
+	element.contents = [start,popover,end] + element.contents[1:]
+
+	#print(len(element.contents),element.contents)
+	#if title in ['ce2887e6-9b07-4f17-bf20-079f4c3f86d8','625a92ad-4c84-4d43-baab-0563fd9ee843']:
+	#	print(f"********{title}**********")
+	#	print('elContent',index,type(element),list(element.children))
+
+
 	return soup
 
 
@@ -281,6 +323,8 @@ def get_point_coordinates(containerPath):
 	'''
 	pattern = r"^(.+)#.*\((.*)\)$"
 	matches = re.search(pattern,containerPath)
+	if matches is None:
+		return [0,0,0]
 
 	document = matches.group(1)
 	components = matches.group(2).split('/')
@@ -302,11 +346,18 @@ def get_point_coordinates(containerPath):
 	return [documentIndex,index,chars]
 
 
+def get_point_coordinates_sort(bookmark):
+	'''
+	for sorting purposes
+	'''
+	return tuple(get_point_coordinates(bookmark.StartContainerPath))
+
+
 def get_bookmark_tag(soup, bookmark):
-	annotation = '<div class=\"text-small mb-3\">\"'+ bookmark.Text.replace("\"","\\\'") + '\"</div><div class=\"fw-bold\">'+  (bookmark.Annotation or '(No comment given.)').replace("\"","\\\'")+'</div>'
+	annotation = '<div class=\"text-small mb-3\">\"'+ bookmark.Text.replace("\"","'") + '\"</div><div class=\"fw-bold\">'+  (bookmark.Annotation or '(No comment given.)').replace("\"","'")+'</div>'
 	tag = soup.new_tag('a')
 	tag["href"] = "javascript:void(0)"
-	tag["class"] = "kne-annotation bg-warning d-inline-block"
+	tag["class"] = "kne-annotation bg-warning"
 	tag["data-bs-toggle"] ="popover"
 	tag["data-bs-placement"] ="bottom"
 	tag["data-bs-trigger"] = "focus"
